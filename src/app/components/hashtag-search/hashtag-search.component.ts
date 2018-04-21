b250bcb65d6f76cf6954d971a439214ce5f64d07
Anasse Zougarh <zougarh.anasse@gmail.com>
2018-04-21 17:09:34 +0200

    ux things

--- src/app/components/hashtag-search/hashtag-search.component.ts
+++ src/app/components/hashtag-search/hashtag-search.component.ts
@@ -14,6 +14,7 @@ export class HashtagSearchComponent implements OnInit {
   messages: Message[];
   loading: boolean;
   postResult: string;
+  noResult: boolean;
 
   constructor(private messageService: MessageService,  private route: ActivatedRoute) {
     this.limit = 5;
@@ -39,6 +40,7 @@ export class HashtagSearchComponent implements OnInit {
     }
     this.messages = [];
     this.loading = true;
+    this.noResult = false;
     let before = new Date();
     this.messageService.getByHashtag(hashtag, this.limit)
         .subscribe(
@@ -47,7 +49,12 @@ export class HashtagSearchComponent implements OnInit {
               let after = new Date();
               this.loading = false;
               let difference = after.getTime() - before.getTime();
-              this.postResult = difference + " milliseconds for getting posts with the hashtag " +  hashtag + " !";
+              if (this.messages) {
+                this.postResult = difference + " milliseconds for getting posts with the hashtag " +  hashtag + " !";
+              } else {
+                this.noResult = true;
+                this.postResult = "No results found for the hashtag " + hashtag +".";
+              }
             },
             error => console.log("Error :: " + error)
         )
@@ -56,6 +63,7 @@ export class HashtagSearchComponent implements OnInit {
   searchByUser(username: string) {
     this.messages = [];
     this.loading = true;
+    this.noResult = false;
     let before = new Date();
     this.messageService.getByUsername(username, this.limit)
         .subscribe(
@@ -64,7 +72,12 @@ export class HashtagSearchComponent implements OnInit {
               let after = new Date();
               this.loading = false;
               let difference = after.getTime() - before.getTime();
-              this.postResult = difference + " milliseconds for getting posts from " +  username + " !";
+              if (this.messages) {
+                this.postResult = difference + " milliseconds for getting posts from " +  username + " !";
+              } else {
+                this.noResult = true;
+                this.postResult = username + " has never posted a post ! ";
+              }  
             },
             error => console.log("Error :: " + error)
         )
